a70b616814282a7e30ec0b65ae26b2190a5d4bfc
Aura Scripcariu <aura.bejenaru@yahoo.com>
2025-09-14 13:22:39 +0300

    Lab 13 - Tema - Teste automate cu Selenium

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        // LoginTest loginTest = new LoginTest();
-        // loginTest.loginWithValidData();
-        // CartTest cartTest = new CartTest();
-        // cartTest.addItemToCart();
+         LoginTest loginTest = new LoginTest();
+         loginTest.loginWithValidData();
 
-        RegisterTest registerTest = new RegisterTest();
-        registerTest.registerUserWithValidCredentials();
+         CartTest cartTest = new CartTest();
+         cartTest.addItemToCart();
+
+         RegisterTest registerTest = new RegisterTest();
+         registerTest.registerUserWithValidCredentials();
     }
 }
