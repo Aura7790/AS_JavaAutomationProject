5f86eb3d9cc5284c8507e8f41b9b02fe8fa84c0a
Aura Scripcariu <aura.bejenaru@yahoo.com>
2025-09-16 17:54:27 +0300

    Lab 14 - Tema 3 - Asserts

--- src/test/java/MainCartTest.java
+++
@@ -1,6 +0,0 @@
-public class MainCartTest {
-    public static void main(String[] args){
-         // CartTest cartTest = new CartTest();
-         // cartTest.addItemToCart();
-    }
-}
--- src/test/java/MainLoginTest.java
+++
@@ -1,6 +0,0 @@
-public class MainLoginTest {
-    public static void main(String[] args){
-        // LoginTest loginTest = new LoginTest();
-        // loginTest.loginWithValidData();
-    }
-}
